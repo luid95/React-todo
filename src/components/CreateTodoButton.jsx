76c9211491f840bef3aa3003084bb67c53f88cfc
lmorales <lmorales@thefactoryhka.com>
2022-02-03 12:47:40 -0600

    Creacion de los eventos para la lista de tareas

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -2,8 +2,18 @@ import React, { Fragment } from 'react';
 import '../css/CreateTodoButton.css';
 
 const CreateTodoButton = (props) => {
+
+    const onClickButton = (msg) => {
+        alert(msg);
+    };
+
     return ( 
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton"
+            onClick={ () => onClickButton('Aqui deberia abrir el modal') }
+        >
+        +
+        </button>
      );
 }
  
--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import '../css/TodoSearch.css';
 
 const TodoSearch = () => {
+
+    const OnSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
+
     return ( 
-        <input className="TodoSearch" placeholder="Cebolla" />
+        <input 
+            className="TodoSearch" 
+            placeholder="Cebolla" 
+            onChange={OnSearchValueChange}
+        />
      );
 }
  
