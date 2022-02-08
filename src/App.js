7117e12b1b0cf51a0d7757eb17a1e3657789fbbc
lmorales <lmorales@thefactoryhka.com>
2022-02-08 10:07:41 -0600

    Manejo de useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppUI } from "./AppUI";
 
 // const defaultTodos =[
@@ -22,40 +22,66 @@ import { AppUI } from "./AppUI";
 
 function useLocalStorage (itemName, initialValue){
 
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [item, setItem] = useState(initialValue);
 
-  if(!localStorageItem){
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
 
-  }else{
-    parsedItem = JSON.parse(localStorageItem);
+        if(!localStorageItem){
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
 
-  }
+        }else{
+          parsedItem = JSON.parse(localStorageItem);
 
-  const [item, setItem] = useState(parsedItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    }, 1000);
+  });
+
 
 
   //funcion para persistir los cambios generados a los TODOs
   const saveItem = ( newItem ) => {
-    const stringifiedItem = JSON.stringify(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
 
-    localStorage.setItem(itemName, stringifiedItem);
+      localStorage.setItem(itemName, stringifiedItem);
 
-    setItem(newItem);
+      setItem(newItem);
+      setLoading(true);
+    } catch (error) {
+      setError(error);
+    }
   };
 
-  return [
+  return {
     item,
-    saveItem
-  ];
+    saveItem,
+    loading,
+    error
+  };
 
 }
 
 function App() {
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos, 
+    saveItem: saveTodos, 
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
 
   //Declaracion de estado
   const [searchValue, setSerchValue] = useState('');
@@ -106,6 +132,8 @@ function App() {
 
   return (
     <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
--- src/AppUI.js
+++ src/AppUI.js
@@ -6,6 +6,8 @@ import { TodoItem } from "./components/TodoItem";
 import { CreateTodoButton } from "./components/CreateTodoButton";
 
 const AppUI = ({
+    loading,
+    error,
     totalTodos,
     completedTodos,
     searchValue,
@@ -28,6 +30,10 @@ const AppUI = ({
             
 
             <TodoList>
+                {error && <p>Desespérate, hubo un error...</p>}
+                {loading && <p>Estamos cargando, no desesperes...</p>}
+                {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+
                 {searchedTodos.map(todo => (
                 
                 <TodoItem 
