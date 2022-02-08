92578a5d96ee7a502ea005003705dcbde83efc72
lmorales <lmorales@thefactoryhka.com>
2022-02-07 22:09:50 -0600

    Custom Hook para Local Storage

--- src/App.js
+++ src/App.js
@@ -20,22 +20,44 @@ import { AppUI } from "./AppUI";
 //   }
 // ];
 
-function App() {
+function useLocalStorage (itemName, initialValue){
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
 
-  if(!localStorageTodos){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
+  if(!localStorageItem){
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
 
   }else{
-    parsedTodos = JSON.parse(localStorageTodos);
+    parsedItem = JSON.parse(localStorageItem);
 
   }
 
+  const [item, setItem] = useState(parsedItem);
+
+
+  //funcion para persistir los cambios generados a los TODOs
+  const saveItem = ( newItem ) => {
+    const stringifiedItem = JSON.stringify(newItem);
+
+    localStorage.setItem(itemName, stringifiedItem);
+
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem
+  ];
+
+}
+
+function App() {
+
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+
   //Declaracion de estado
-  const [todos, setTodos] = useState(parsedTodos);
   const [searchValue, setSerchValue] = useState('');
 
   //Verificar  todos completed and all todos
@@ -60,15 +82,6 @@ function App() {
     });
     
   }
-
-  //funcion para persistir los cambios generados a los TODOs
-  const saveTodos = ( newTodos ) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-
-    setTodos(newTodos);
-  };
 
   //funcion para marcar completar y eliminar Todos
   const completeTodo = (text) => {
